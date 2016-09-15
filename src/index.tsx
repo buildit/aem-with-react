55d596603a5847c1605ba59ef2939d44c3226e52
Panos Voudouris <panosvoudouris@gmail.com>
2016-09-15 11:56:59 +0100

    Move output to dist. This is a breaking change

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import BaseComponent from './BaseComponent';
+import ComponentManager from './ComponentManager';
+import ComponentRegistry from './ComponentRegistry';
+import RootComponentRegistry from './RootComponentRegistry';
+import ReactParsys from './component/ReactParsys';
+
+export {
+    BaseComponent,
+    ComponentManager,
+    ComponentRegistry,
+    RootComponentRegistry,
+    ReactParsys
+};
